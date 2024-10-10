d655145c14d2b599f04289fbf9d27d84fd94ffcb
siddhesh904 <siddheshwarkapde20@gmail.com>
2024-10-10 18:24:37 +0530

    Todays work done

--- MyProjEcom/App/views.py
+++ MyProjEcom/App/views.py
@@ -2,13 +2,27 @@
 from django.shortcuts import render,redirect
 from App.models import *
 from decimal import Decimal
-
+from django.http import HttpResponse
 # Create your views here.
 
 
 def index(request):
+    mainCat = MainCategory.objects.get(name = 'Arts & Crafts')
+    print(mainCat)
+    SubCat_data = [ 'Painting, Drawing & Art Supplies'
+'Beading & Jewelry Making','Crafting','Fabric','Fabric Decorating','Knitting & Crochet','Needlework','Organization, Storage & Transport','Printmaking','Scrapbooking & Stamping','Sewing','Party Decorations & Supplies','Gift Wrapping Supplies' ]
+    print(SubCat_data)
+    
+    # if(mainCat):
+    #     for name in SubCat_data:
+    #         SubCategory.objects.create(
+    #             name = name,
+    #             main_category = mainCat
+    #         )
+    # else:
+    #     return HttpResponse("Data already sotred!")
+        
     return render(request, 'home.html')
-
 
 
 
@@ -23,27 +37,37 @@ def Addproduct(request):
         product_stock = request.POST.get('product_stock')
         main_category_id = request.POST.get('category')
         sub_category_id = request.POST.get('subcategory')
-        image = request.FILES.get('images')
-        
-        
-       
-        product_price = Product.objects.values_list('price', flat = True)
-        product_stock = Product.objects.values_list('stock', flat = True)
-        print(product_stock)
+        image = request.FILES.get('images')    
         
         if(main_category_id):
             main_category_selected = MainCategory.objects.get(id = main_category_id)
             sub_categories = SubCategory.objects.filter(main_category = main_category_selected)
             
             
-        if(sub_category_id and product_name and product_description ):
+        if(sub_category_id and product_name and product_description and product_stock and product_price ):
             main_category = MainCategory.objects.get(id = main_category_id)
             sub_category = SubCategory.objects.get(id = sub_category_id)
             
+            
+            queryset = Product.objects.create(
+                name = product_name,
+                description = product_description,
+                price = product_price,
+                stock = product_stock,
+                main_category = main_category,
+                sub_category = sub_category,
+                image = image,
+            )
+            
+            queryset.save()
             print(main_category)
             print(sub_category)
             print(product_name)
             print(product_description)
+            print(product_stock)
+            print(product_price)
+            print(image)
+            
             
             
             return redirect('/form')
